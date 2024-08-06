b1c0086fd88a1e898aac9dc866bd5b036ac3dd9e
Shyam <ShyamSundar.M4@cognizant.com>
2024-08-06 15:37:22 +0530

    Migrating to discoveryClient with updated spring cloud version for Java 17

--- spring/microservices/taskservice/src/main/java/com/taskmanagement/taskservice/TaskserviceApplication.java
+++ spring/microservices/taskservice/src/main/java/com/taskmanagement/taskservice/TaskserviceApplication.java
@@ -2,10 +2,10 @@ package com.taskmanagement.taskservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class TaskserviceApplication {
 
 	public static void main(String[] args) {
