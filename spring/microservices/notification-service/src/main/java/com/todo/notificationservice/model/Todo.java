4b75dc2d04df3e72091bb423f2a9ebfd9c5cfc49
Shyam <shaamsundar16@gmail.com>
2024-08-09 12:20:36 +0530

    Modified POJO to retrive document from database

--- spring/microservices/notification-service/src/main/java/com/todo/notificationservice/model/Todo.java
+++ spring/microservices/notification-service/src/main/java/com/todo/notificationservice/model/Todo.java
@@ -4,10 +4,13 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Document(collection = "todos")
 public class Todo {
     @Id
     private String id;
