4ec57cb4eec671fd67350ccb35810de4cdf2f83f
Shyam <ShyamSundar.M4@cognizant.com>
2024-08-21 17:00:39 +0530

    Initial design for task list page

--- angular/todo-app/src/app/components/todo-list/todo-list.component.ts
+++ angular/todo-app/src/app/components/todo-list/todo-list.component.ts
@@ -11,7 +11,7 @@ import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styles: []
+  styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
   displayedColumns: string[] = ['title', 'description', 'effortRequired', 'category', 'status'];
@@ -28,11 +28,22 @@ export class TodoListComponent implements OnInit {
   }
 
   getTasks() {
-    this.todoService.getTaskByUserId(this.userService.user.email).subscribe((res: any) => {
-      this.todoList = res;
-      this.dataSource.data = res;
-      this.dataSource.paginator = this.paginator;
-    });
+    const sampleTasks = [
+      { id: 1, title: 'Task 1', description: 'Description for Task 1', effortRequired: 1, category: 'Developement', status: 'IN_PROGRESS' },
+      { id: 2, title: 'Task 2', description: 'Description for Task 2' , effortRequired: 1, category: 'Developement', status: 'COMPLETED' },
+      { id: 3, title: 'Task 3', description: 'Description for Task 3' , effortRequired: 1, category: 'Developement', status: 'PENDING' },
+      { id: 4, title: 'Task 4', description: 'Description for Task 4' , effortRequired: 1, category: 'Developement', status: 'COMPLETED' },
+      { id: 5, title: 'Task 5', description: 'Description for Task 5' , effortRequired: 1, category: 'Developement', status: 'PENDING' }
+    ];
+  
+    this.dataSource.data = sampleTasks;
+    this.dataSource.paginator = this.paginator;
+  
+    // this.todoService.getTaskByUserId(this.userService.user.email).subscribe((res: any) => {
+    //   this.todoList = res;
+    //   this.dataSource.data = res;
+    //   this.dataSource.paginator = this.paginator;
+    // });
   }
 
 }
