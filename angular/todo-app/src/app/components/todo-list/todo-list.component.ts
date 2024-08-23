e9e45f66cc0e86aa8e61112e1dc3f054407dd2c2
Shyam <shaamsundar16@gmail.com>
2024-08-23 21:52:38 +0530

    Fixed favorites page

--- angular/todo-app/src/app/components/todo-list/todo-list.component.ts
+++ angular/todo-app/src/app/components/todo-list/todo-list.component.ts
@@ -58,26 +58,25 @@ export class TodoListComponent implements OnInit, AfterViewInit {
     this.updatePaginatedTasks(); 
   }
   getTasks() {
-    const sampleTasks = [
-      { id: 1, title: 'Task 1', description: 'Description for Task 1', effortRequired: 1, category: 'Developement', status: 'IN_PROGRESS', startDate: '21/08/24', endDate: '29/08/24', isFavorite: true },
-      { id: 2, title: 'Task 2', description: 'Description for Task 2' , effortRequired: 1, category: 'Developement', status: 'COMPLETED', startDate: '21/08/24', endDate: '29/08/24', isFavorite: false },
-      { id: 3, title: 'Task 3', description: 'Description for Task 3' , effortRequired: 1, category: 'Developement', status: 'PENDING' , startDate: '21/08/24', endDate: '29/08/24', isFavorite: true},
-      { id: 4, title: 'Task 4', description: 'Description for Task 4' , effortRequired: 1, category: 'Developement', status: 'COMPLETED' , startDate: '21/08/24', endDate: '29/08/24', isFavorite: false},
-      { id: 5, title: 'Task 5', description: 'Description for Task 5' , effortRequired: 1, category: 'Developement', status: 'PENDING', startDate: '21/08/24', endDate: '29/08/24', isFavorite: true},
-      { id: 3, title: 'Task 3', description: 'Description for Task 3' , effortRequired: 1, category: 'Developement', status: 'PENDING' , startDate: '21/08/24', endDate: '29/08/24', isFavorite: false},
-      { id: 4, title: 'Task 4', description: 'Description for Task 4' , effortRequired: 1, category: 'Developement', status: 'COMPLETED' , startDate: '21/08/24', endDate: '29/08/24', isFavorite: false},
-      { id: 5, title: 'Task 5', description: 'Description for Task 5' , effortRequired: 1, category: 'Developement', status: 'PENDING' , startDate: '21/08/24', endDate: '29/08/24', isFavorite: false}
-    ];
-    this.dataSource.data = sampleTasks;
-    this.updatePaginatedTasks();
+    // const sampleTasks = [
+    //   { id: 1, title: 'Task 1', description: 'Description for Task 1', effortRequired: 1, category: 'Developement', status: 'IN_PROGRESS', startDate: '21/08/24', endDate: '29/08/24', isFavorite: true },
+    //   { id: 2, title: 'Task 2', description: 'Description for Task 2' , effortRequired: 1, category: 'Developement', status: 'COMPLETED', startDate: '21/08/24', endDate: '29/08/24', isFavorite: false },
+    //   { id: 3, title: 'Task 3', description: 'Description for Task 3' , effortRequired: 1, category: 'Developement', status: 'PENDING' , startDate: '21/08/24', endDate: '29/08/24', isFavorite: true},
+    //   { id: 4, title: 'Task 4', description: 'Description for Task 4' , effortRequired: 1, category: 'Developement', status: 'COMPLETED' , startDate: '21/08/24', endDate: '29/08/24', isFavorite: false},
+    //   { id: 5, title: 'Task 5', description: 'Description for Task 5' , effortRequired: 1, category: 'Developement', status: 'PENDING', startDate: '21/08/24', endDate: '29/08/24', isFavorite: true},
+    //   { id: 3, title: 'Task 3', description: 'Description for Task 3' , effortRequired: 1, category: 'Developement', status: 'PENDING' , startDate: '21/08/24', endDate: '29/08/24', isFavorite: false},
+    //   { id: 4, title: 'Task 4', description: 'Description for Task 4' , effortRequired: 1, category: 'Developement', status: 'COMPLETED' , startDate: '21/08/24', endDate: '29/08/24', isFavorite: false},
+    //   { id: 5, title: 'Task 5', description: 'Description for Task 5' , effortRequired: 1, category: 'Developement', status: 'PENDING' , startDate: '21/08/24', endDate: '29/08/24', isFavorite: false}
+    // ];
+    // this.dataSource.data = sampleTasks;
+    // this.updatePaginatedTasks();
   
-    // this.todoService.getTaskByUserId(this.userService.user.email).subscribe((res: any) => {
-    //   this.todoList = res;
-    //   console.log(res);
-    //   this.dataSource.data = res;
-    //   this.dataSource.paginator = this.paginator;
-    //   this.updatePaginatedTasks();
-    // });
+    this.todoService.getTaskByUserId(this.userService.user.email).subscribe((res: any) => {
+      this.todoList = res;
+      this.dataSource.data = res;
+      this.dataSource.paginator = this.paginator;
+      this.updatePaginatedTasks();
+    });
     
   }
   updatePaginatedTasks() {
@@ -92,7 +91,6 @@ export class TodoListComponent implements OnInit, AfterViewInit {
   toggleSelected(task) {
     task.isFavorite = !task.isFavorite;
     this.todoService.updateTask(task).subscribe((res: any) => {
-       console.log(res);
     });
   }
   
